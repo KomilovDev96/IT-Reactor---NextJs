c4723cdc20a5156dd9c2d5947f1a9c186155ee8e
Azizbek <46714958+Azizbek1@users.noreply.github.com>
2023-07-01 21:55:13 +0500

    Langue Bitdi

--- client/app/assets/index.ts
+++ client/app/assets/index.ts
@@ -5,9 +5,11 @@ import slide1 from "./jpg/nubelson-fernandes-iE71-TMrrkE-unsplash.jpg"
 // SVG 
 import ru from './svg/Ru.svg'
 import uz from './svg/Uz.svg'
+import lang from './svg/language.svg'
 export {
     slide1,
     about1,
+    lang,
     ru,
     uz
 }
